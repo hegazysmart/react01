be024962eea727a1503f4360c461062c92f9e0d4
Mohamed Hegazy <hegazysmart@gmail.com>
2021-03-20 05:02:30 +0200

    Delete lecture directory

--- lecture/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <>
-      <h6>Footer</h6>
-    </>
-  );
-};
-
-export default Footer;
--- lecture/src/components/UsersCount.jsx
+++
@@ -1,4 +0,0 @@
-const UsersCount = ({count}) => {
-  return <p className="text-primary font-weight-bold">Users Count : {count}</p>;
-};
-export default UsersCount;
